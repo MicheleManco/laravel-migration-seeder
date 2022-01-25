406ff3fb9bea2e381052dd37cad6689a951db336
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2022-01-25 15:40:31 +0100

    create: migrations and albums,artists done

---
+++ database/migrations/2022_01_25_141945_create_artists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('surname', 60);
+            $table->tinyInteger('age');
+            $table->date('date_of_birth');
+            $table->string('stage_name', 60)->unique();
+            $table->string('record_company', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+}
---
+++ database/migrations/2022_01_25_142004_create_albums_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',60);
+            $table->tinyInteger('n_songs');
+            $table->integer('sold');
+            $table->string('record_company', 100);
+            $table->string('genres', 60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
